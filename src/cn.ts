8dc615ee1014241f2ba1211bdb3e19480e1da10b
Bradley <15671567+bradleyhodges@users.noreply.github.com>
2026-09-06 18:20:51 +0800

    feat(src): replace `tailwind-merge` and `clsx` with `cn`

--- src/cn.ts
+++ src/cn.ts
@@ -1,10 +1,7 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
 /**
  * Combines multiple class names into a single string, merging Tailwind CSS classes intelligently.
  *
  * @param {...ClassValue[]} inputs - An array of class values to be combined.
  * @returns {string} - A single string containing the combined class names.
  */
-export const cn = (...inputs: ClassValue[]): string => twMerge(clsx(inputs));
+export { cn } from "cn"
