915190e8cb060443eee014ef7060b3a2177f850a
Bradley <15671567+bradleyhodges@users.noreply.github.com>
2026-09-06 18:21:46 +0800

    chore: run linter/formatter

--- src/cn.ts
+++ src/cn.ts
@@ -4,4 +4,4 @@
  * @param {...ClassValue[]} inputs - An array of class values to be combined.
  * @returns {string} - A single string containing the combined class names.
  */
-export { cn } from "cn"
+export { cn } from "cn";
